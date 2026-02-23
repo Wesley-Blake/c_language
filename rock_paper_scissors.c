b3ad03330737dda56d7ed308d01602db926c592d
= <=>
2026-02-22 17:09:52 -0800

    I don't think there is anything else I can do to this.

--- rock_paper_scissors.c
+++ rock_paper_scissors.c
@@ -2,55 +2,35 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int processBuffer(char *buffer, size_t size);
+// Trying out functions.
+int process_buffer(char *buffer, size_t size);
+// I just wanted to make another function :D.
+void process_input(char input);
+// Clear screen and detects operating system.
+void clear_screen(void);
 
 int main(void) {
-    bool loop = true;
-    char buffer[10]; // 10 because I ready it is convention, but 2 should do.
-    char input = 'y'; // Shouldn't be an option.
-    int cpu = 0; // I don't care if this is an option.
+    // 10 because I ready it is convention, but 2 should do.
+    char buffer[10];
+    // I don't want this value default value to be valid.
+    char input = 'y';
 
-    while (loop) {
+    // The game!
+    while (true) {
         printf("q: quit, s: scissors, r: rock, p: paper\n");
-        processBuffer(buffer, sizeof(buffer) / sizeof(buffer[0]));
-        
+        process_buffer(buffer, sizeof(buffer) / sizeof(buffer[0]));
         input = buffer[0];
-        cpu = rand() % 3;
-
-        switch (input) {
-            case 'q':
-                printf("Thanks for playing!");
-                loop = false;
-                break;
-            case 's':
-                // logic
-                if (cpu == 0) printf("cpu: s tie!");
-                if (cpu == 1) printf("cpu: r You lost!");
-                if (cpu == 2) printf("cpu: p You win!");
-                break;
-            case 'r':
-                // logic
-                if (cpu == 0) printf("cpu: s You win!");
-                if (cpu == 1) printf("cpu: r tie!");
-                if (cpu == 2) printf("cpu: p You lost!");
-                break;
-            case 'p':
-                // logic
-                if (cpu == 0) printf("cpu: s You lost!");
-                if (cpu == 1) printf("cpu: r You win!");
-                if (cpu == 2) printf("cpu: p tie!");
-                break;
-            default:
-                printf("This seems to be invalide input!");
-                break;
+        if (input == 'q') {
+            printf("Thanks for playing!\n");
+            break;
         }
-        printf("\n\n");
+        clear_screen();
+        process_input(input);
     }
-
     return 0;
 }
 
-int processBuffer(char *buffer, size_t size) {
+int process_buffer(char *buffer, size_t size) {
     // Safe input collection.
     if (fgets(buffer, size, stdin) == NULL){
         printf("Input error, sorry.\n");
@@ -58,4 +38,40 @@ int processBuffer(char *buffer, size_t size) {
     // Skips all the nonsense till I see a new line.
     if (buffer[1] != '\n') while (getchar() != '\n');
     return 0;
+}
+
+void process_input(char input) {
+    int cpu = rand() % 3;
+    switch (input) {
+        case 's':
+            // logic
+            if (cpu == 0) printf("cpu: s tie!");
+            if (cpu == 1) printf("cpu: r You lost!");
+            if (cpu == 2) printf("cpu: p You win!");
+            break;
+        case 'r':
+            // logic
+            if (cpu == 0) printf("cpu: s You win!");
+            if (cpu == 1) printf("cpu: r tie!");
+            if (cpu == 2) printf("cpu: p You lost!");
+            break;
+        case 'p':
+            // logic
+            if (cpu == 0) printf("cpu: s You lost!");
+            if (cpu == 1) printf("cpu: r You win!");
+            if (cpu == 2) printf("cpu: p tie!");
+            break;
+        default:
+            printf("This seems to be invalide input!");
+            break;
+    }
+    printf("\n\n");
+}
+
+void clear_screen(void) {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
